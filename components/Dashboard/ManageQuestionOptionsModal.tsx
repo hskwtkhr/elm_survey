42d844938e25281c32ab19fc410fdf551a97ac28
cleardesignhouse <gooro_@yahoo.co.jp>
2025-12-06 07:22:15 +0900

    Fix: Add missing embedded check to ManageQuestionOptionsModal

--- components/Dashboard/ManageQuestionOptionsModal.tsx
+++ components/Dashboard/ManageQuestionOptionsModal.tsx
@@ -396,6 +396,19 @@ export default function ManageQuestionOptionsModal({
         
     </div>
   )
+
+  if (embedded) {
+    return content
+  }
+
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      onClick={handleClose}
+    >
+      {content}
+    </div>
+  )
 }
 
 
