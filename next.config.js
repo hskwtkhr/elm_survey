8d56c2460d24cbaa1002e222eba48883128abc33
cleardesignhouse <gooro_@yahoo.co.jp>
2025-12-05 13:36:51 +0900

    Add CSS version v4 and cache control headers to force cache refresh

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
-
-
